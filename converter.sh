6d542a0fe98b80db48dbf870eb0d0b5c551bd066
Mark Lautman <mlautman@perfectsensedigital.com>
2020-03-20 10:24:52 -0400

    Initial commit.

---
+++ converter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Perform the following preprocessing on the HTML files before running this script:
+
+# The entity name must immediately follow the '&' in the entity reference.
+# To resolve:
+#   Replace any & that is not an HTML entity with nothing
+
+#The element type "link" must be terminated by the matching end-tag "</link>".
+# Close opened <link> tags
+# Close opened <img> tags
+
+# Attribute name "data-docsearch-input" associated with an element type "input" must be followed by the ' = ' character.
+
+# Find attributes with tags that have no = and delete them.
+# Remove namespace declaraiton in <html> tag
+
+saxon -s:/Users/mlautman/Documents/paligo/converter/test.html -xsl:/Users/mlautman/Documents/paligo/converter/converter.xsl -o:docbook.xml
+
